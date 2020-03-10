650278565f6c1531a2121d84f0a298a161228dd8
Lucas de Sousa Assunção <lucasdesousa19@hotmail.com>
2020-03-10 20:03:47 +0000

    50 start

--- secao-7/src/Routes.js
+++ secao-7/src/Routes.js
@@ -2,10 +2,11 @@ import React from 'react';
 import {BrowserRouter, Route, Switch } from 'react-router-dom'
 
 import Home from './pages/Home'
-
+import Header from './components/Header'
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Switch>
         <Route exact path='/' component={Home} />
       </Switch>
--- secao-7/src/pages/Home.js
+++ secao-7/src/pages/Home.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Home = () =>{
   return(
-    <h2>Home</h2>
+    <h2>Pagina Home</h2>
   )
 }
 
